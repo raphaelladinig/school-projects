299910b0eccff3e96d181e7a09a04ddfec686dc0
Raphael Ladinig <ra.ladinig@gmail.com>
2025-03-03 12:04:57 +0100

    reiselogbuch

---
+++ reiselogbuch/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello World!",
+    };
+  },
+};
+</script>
+
+<style></style>
