a70c8b706cc7de9bbb4f6cf34072c38c27606a50
Raphael Ladinig <ra.ladinig@gmail.com>
2025-03-17 11:37:03 +0100

    klotz

--- reiselogbuch/.nvim.lua
+++ reiselogbuch/.nvim.lua
@@ -1,9 +1,5 @@
-local function load_languages(languages)
-	for _, lang in ipairs(languages) do
-		dofile(vim.fn.stdpath("config") .. "/lua/languages/" .. lang .. ".lua")
-	end
-end
-
-load_languages({ "vue", "html", "css" })
+require("vue")
+require("html")
+require("css")
 
 print("successfully loaded .nvim.lua")
